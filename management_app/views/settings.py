1e5610598ec4e4710a614796a8bc78103c818be4
Justin Lock <34179707+jjlock@users.noreply.github.com>
2022-11-20 21:09:03 -0800

    Move get_db calls to helper functions

--- management_app/views/settings.py
+++ management_app/views/settings.py
@@ -8,11 +8,10 @@ settings = Blueprint('settings', __name__, url_prefix='/settings')
 @settings.route('/admins', methods=['GET', 'POST'])
 @login_required
 def index():
-    db = get_db()
     if request.method == 'POST':
-        add_admin_status(db, request.form['user-select'])
+        add_admin_status(request.form['user-select'])
 
-    users, admins = get_users_and_admins(db)
+    users, admins = get_users_and_admins()
     current_user = next(admin for admin in admins if admin['user_email'] == session['email'])
 
     return render_template(
@@ -25,14 +24,13 @@ def index():
 @settings.route('/admins/<id>', methods=['POST', 'DELETE'])
 @login_required
 def remove_admin(id):
-    db = get_db()
-    if is_only_admin(db):
+    if is_only_admin():
         flash('Cannot remove admin when only one is left.', 'error')
         return make_response('Cannot remove admin when only one is left.', 400)
     if request.method == 'POST':
-        remove_admin_status(db, id)
+        remove_admin_status(id)
     if request.method == 'DELETE':
-        delete_admin(db, id)
+        delete_admin(id)
     return Response(status=200)
     
 
@@ -46,11 +44,11 @@ def point_policy():
 @settings.route('/point-policy/rules/<id>', methods=['POST'])
 @login_required
 def edit_rule_point_value(id):
-    db = get_db()
-    update_rule_point_value(db, id, request.form['point-value'])
+    update_rule_point_value(id, request.form['point-value'])
     return redirect(url_for('settings.point_policy'))
 
-def get_users_and_admins(db):
+def get_users_and_admins():
+    db = get_db()
     admins = []
     users = []
     res = db.execute("""
@@ -66,22 +64,27 @@ def get_users_and_admins(db):
 
     return (users, admins)
 
-def add_admin_status(db, id):
+def add_admin_status(id):
+    db = get_db()
     db.execute('UPDATE users SET admin = 1 WHERE user_id = ?', (id))
     db.commit()
 
-def remove_admin_status(db, id):
+def remove_admin_status(id):
+    db = get_db()
     db.execute('UPDATE users SET admin = 0 WHERE user_id = ?', (id))
     db.commit()
 
-def delete_admin(db, id):
+def delete_admin(id):
+    db = get_db()
     db.execute('DELETE FROM users WHERE user_id = ?', (id))
     db.commit()
 
-def is_only_admin(db):
+def is_only_admin():
+    db = get_db()
     res = db.execute('SELECT COUNT(admin) AS count FROM users WHERE admin IS TRUE').fetchone()
     return res['count'] == 1
 
-def update_rule_point_value(db, id, value):
+def update_rule_point_value(id, value):
+    db = get_db()
     db.execute('UPDATE rules SET value = ? WHERE rule_id = ?', (value, id))
     db.commit()
