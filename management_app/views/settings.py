77e214f2e2013d6c301c1d69a8a56ef5fa38260d
Justin Lock <34179707+jjlock@users.noreply.github.com>
2022-11-30 17:49:00 -0800

    Log actions on settings pages

--- management_app/views/settings.py
+++ management_app/views/settings.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, Response, render_template, flash, make_response, redirect, url_for, session, request
 
-from management_app.views.auth import login_required
 from management_app.db import get_db
+from management_app.views.auth import login_required
+from management_app.views.utils import insert_log
 
 settings = Blueprint('settings', __name__, url_prefix='/settings')
 
@@ -68,16 +69,19 @@ def add_admin_status(id):
     db = get_db()
     db.execute('UPDATE users SET admin = 1 WHERE user_id = ?', (id,))
     db.commit()
+    insert_log(session['name'], id, None, 'Add new admin')
 
 def remove_admin_status(id):
     db = get_db()
     db.execute('UPDATE users SET admin = 0 WHERE user_id = ?', (id,))
     db.commit()
+    insert_log(session['name'], id, None, 'Remove admin status')
 
 def delete_admin(id):
     db = get_db()
     db.execute('DELETE FROM users WHERE user_id = ?', (id,))
     db.commit()
+    insert_log(session['name'], id, None, 'Delete admin')
 
 def is_only_admin():
     db = get_db()
@@ -86,5 +90,8 @@ def is_only_admin():
 
 def update_rule_point_value(id, value):
     db = get_db()
+    old = db.execute('SELECT value FROM rules WHERE rule_id = ?', (id,)).fetchone()
     db.execute('UPDATE rules SET value = ? WHERE rule_id = ?', (value, id))
     db.commit()
+    new = db.execute('SELECT * FROM rules WHERE rule_id = ?', (id,)).fetchone()
+    insert_log(session['name'], None, None, f'Update point policy - Rule Name: {new["rule_name"]} | Value: {old["value"]} -> {new["value"]}')
